f0f22b5175318d7cbd47c86894ebd7c22dd38132
Ross Perry <perryr16@gmail.com>
2020-03-18 13:14:27 -0600

    Complete iteration 1 and 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,22 @@
+require 'pry'
+class Restaurant
 
+  attr_reader :opening_time, :name, :dishes
+
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(time)
+  #  binding.pry
+    closing_int = opening_time.to_i + time
+    @closing_time = closing_int.to_s + ":00"
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+end
